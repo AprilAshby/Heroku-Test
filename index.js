2c9107599acd21e8814422a2ac8d7406f96bc4ca
April Darger <april.m.darger@gmail.com>
2020-12-03 11:36:34 -0700

    heroku package.json.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 4000
+const path = require('path') // comes shipped with node, but needs to be loaded. (so we don't need to npm i)
 
 console.log('web 36 rocks')
 console.log(__dirname)
@@ -18,10 +19,16 @@ console.log(process.env.PORT)
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build'))) // static assets served from
 
 // api, routers
 app.use('/api/*', (_, res) => {
   res.json({ data: 'web 36 rocks'})
+})
+
+app.use('*', (_, res) => {
+  // send back index.html
+  res.sendFile(path.join(__dirname, 'client/build/index.html')) // if no path found route to index.html
 })
 
 app.listen(port, () => {
